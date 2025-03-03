8159739e872c493c56db0496e8cf6fb2c4623294
Haaju14 <phuchau22923@gmail.com>
2025-03-03 08:29:37 +0700

    push

--- src/App.jsx
+++ src/App.jsx
@@ -25,31 +25,10 @@ const App = () => (
     onFinishFailed={onFinishFailed}
     autoComplete="off"
   >
-    <Form.Item
-      label="Username"
-      name="username"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your username!',
-        },
-      ]}
-    >
-      <Input />
-    </Form.Item>
+    
 
-    <Form.Item
-      label="Password"
-      name="password"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your password!',
-        },
-      ]}
-    >
-      <Input.Password />
-    </Form.Item>
+
+    
 
     <Form.Item name="remember" valuePropName="checked" label={null}>
       <Checkbox>Remember me</Checkbox>
